67569afad1aab25e278eb9a673f54de1d7870a9f
Jessica Bonnefon <jessbonne97@gmail.com>
2023-09-10 23:35:45 -0300

    Add Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center space-y-10 home-background">
+      Home
+    </div>
+  )
+}
+
+export default Home;
